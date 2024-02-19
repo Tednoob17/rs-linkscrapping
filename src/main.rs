41d4df745f7942ed270683d49f68ec1ca8a7ec9b
Ted Kouhouenou <teddams047@gmail.com>
2024-02-19 16:12:38 +0100

    add imp lib

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,3 @@ use error_chain::error_chain;
 use select::document::Document;
 use select::predicate::Name;
 
-error_chain! {
-    foreign_links{
-        ReqError(reqwest::Error);
-        IoError(std::io::Error);
-    }
-}
-#[tokio::main]
-async fn main() -> Result <()> {
-
-        let res = reqwest::get(
-            "https://accelerator.github.com/"
-        ).await?
-        .text()
-        .await?;
-
-        println!("Links on web page 'accelerator.github.com' : ");
-
-        Document::from(res.as_str())
-        .find(Name("a"))
-        .filter_map(|n|n.attr("href"))
-        .for_each(|x| println!("{}", x));
-
-        Ok(())
-}
