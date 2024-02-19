a7f1f7ab317ef4761171d91ce35f72010d31481e
Ted Kouhouenou <teddams047@gmail.com>
2024-02-19 16:13:40 +0100

    Err from reqwest and io

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,9 @@ use error_chain::error_chain;
 use select::document::Document;
 use select::predicate::Name;
 
+error_chain! {
+    foreign_links{
+        ReqError(reqwest::Error);
+        IoError(std::io::Error);
+    }
+}
