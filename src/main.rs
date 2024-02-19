d7aaca742111724809cb653abfea15a3a315b752
Ted Kouhouenou <teddams047@gmail.com>
2024-02-19 16:28:01 +0100

    add args funct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use error_chain::error_chain;
 use select::document::Document;
 use select::predicate::Name;
+use std::env;
+
 
 error_chain! {
     foreign_links{
@@ -8,4 +10,24 @@ error_chain! {
         IoError(std::io::Error);
     }
 }
-#[tokio:main]
+#[tokio::main]
+async fn main() -> Result <()> {
+        let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <URL>", args[0]);
+        return Ok(());
+    }
+
+        let  link= &args[1];
+
+        let res = reqwest::get(link).await?.text().await?;
+
+        println!("Links on web page '{}' :", link);
+
+        Document::from(res.as_str())
+        .find(Name("a"))
+        .filter_map(|n|n.attr("href"))
+        .for_each(|x| println!("{}", x));
+
+        Ok(())
+}
